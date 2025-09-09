d1fb66890fddb35a4a30c9dbacbba66a9c50dccf
ex_yangfan.wu <ex_yangfan.wu@tcl.com>
2025-09-09 10:22:43 +0800

    feat: 插入排序代码提交

--- solidity/insertSort.sol
+++ solidity/insertSort.sol
@@ -1,2 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity version;
+pragma solidity ^0.8.26;
+
+contract MySort {
+    function sort(uint256[] memory a) public pure returns (uint256[] memory) {
+        uint256 aLen = a.length;
+        for (uint256 i = 1; i < aLen; i++) {
+            uint256 cur = a[i];
+            uint256 l = 0;
+            uint256 r = i;
+            while (l < r) {
+                uint256 m = (l + r) >> 1;
+                if (cur >= a[m]) {
+                    l = m + 1;
+                } else {
+                    r = m;
+                }
+            }
+            for (uint256 j = i; j > l; j--) {
+                a[j] = a[j - 1];
+            }
+            a[l] = cur;
+        }
+        return a;
+    }
+}
