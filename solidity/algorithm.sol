a54cdfa892a6eb9b495285ecf448b6a1716e193c
leno23 <1102042793@qq.com>
2025-09-10 11:32:24 +0800

    feat: 添加注释

--- solidity/algorithm.sol
+++ solidity/algorithm.sol
@@ -5,8 +5,12 @@ contract Algo {
     function reverseStr(string memory str) public pure returns (string memory) {
         uint256 len = bytes(str).length;
         bytes memory res = new bytes(len);
-        for (uint256 i = len; i > 0; i--) {
-            res[len - i] = bytes(str)[i - 1];
+
+        // 保证不出错，跳过检查，节省gas
+        unchecked{
+            for (uint256 i = len; i > 0; i--) {
+                res[len - i] = bytes(str)[i - 1];
+            }
         }
         return string(res);
     }
